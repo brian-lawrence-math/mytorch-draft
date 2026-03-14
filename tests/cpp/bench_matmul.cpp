f485d4486341f7f804d5094996ff3db7ca1240c0
Brian Lawrence <brian.lawrence.math@gmail.com>
2026-03-14 11:28:46 -0700

    Add C++ benchmark

---
+++ tests/cpp/bench_matmul.cpp
@@ -0,0 +1,37 @@
+
+#include <cuda_runtime.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include <iostream>
+#include <cassert>
+
+#include "cuda_utils.h"
+#include "tensor.h"
+
+void bench_matmul() {
+	FloatTensor t1 = FloatTensor::randn({100, 1000, 1000});
+	FloatTensor t2 = FloatTensor::randn({100, 1000, 1000});
+
+	t1.move_to_device(Device::GPU);
+	t2.move_to_device(Device::GPU);
+
+	cudaDeviceSynchronize();
+	auto start = std::chrono::steady_clock::now();
+
+	FloatTensor res = t1.matmul_3d(t2);
+
+	cudaDeviceSynchronize();
+	auto stop = std::chrono::steady_clock::now();
+
+	std::chrono::duration<double> elapsed = stop - start;
+	std::cout << "Time elapsed: " << elapsed.count() << std::endl;
+}
+
+int main() {
+	bench_matmul();
+	return 0;
+}
+
+	
